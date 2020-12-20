e249cf4850f16e6c9a52a8f51afc6767a376d799
Carina Rebeca <carina.plisco@gmail.com>
2020-12-21 00:55:18 +0200

    problema 1 // cate nr sunt pare

---
+++ problema1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //se da o secventa de n numere.Sa se determine cate din ele sunt pare. 
+
+            int n, nr, i, par;
+
+            Console.WriteLine("Cate numere doriti sa introduceti in secventa?");
+            Console.Write("n = ");
+            n = int.Parse(Console.ReadLine());
+
+            par = 0;
+
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("numarul {0}  = ", i);
+                nr = int.Parse(Console.ReadLine());
+
+                if (nr % 2 == 0)
+                    par++;
+            }
+                         
+            Console.WriteLine($"In secventa sunt {par} numere pare.");
+        }
+    }
+}
