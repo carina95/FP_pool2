77d77de157e72f69b51793307e093dd8223b90e3
Carina Rebeca <carina.plisco@gmail.com>
2020-12-29 00:16:10 +0200

    eg cu pozitia pe care apar

---
+++ problema 5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cate elemente dintr-o secventa de n numere sunt egale cu pozitia pe care apar in secventa. 
+            //Se considera ca primul element din secventa este pe pozitia 0. 
+
+            int n, i, nr, eg;
+
+            Console.WriteLine(" Cate numere doriti sa introduceti in secventa?");
+            Console.Write(" n = ");
+            n = int.Parse(Console.ReadLine());
+
+            eg = 0;
+
+            for (i = 0; i <= n; i++)
+            {
+                Console.Write("numarul {0}  = ", i);
+                nr = int.Parse(Console.ReadLine());
+
+                if (nr == i)
+                {
+                    eg++;
+                }
+            }
+
+            Console.WriteLine($"In secventa data, {eg} numere sunt egale cu pozitia pe care apar.");
+        }
+    }
+}
