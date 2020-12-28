22860917253c7b1b7f9605112c3d401c562fcef1
Carina Rebeca <carina.plisco@gmail.com>
2020-12-29 00:10:26 +0200

    problema 4 // cautare a in secv

---
+++ problema 4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se da o secventa de n numere. Determinati pe ce pozitie se afla in secventa un numar a. 
+            //Se considera ca primul element din secventa este pe pozitia zero. 
+            //Daca numarul nu se afla in secventa raspunsul va fi -1. 
+
+            int n, nr, i, a, c;
+
+            Console.WriteLine(" Cate numere doriti sa introduceti in secventa?");
+            Console.Write(" n = ");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Ce numar doriti sa cautati in secventa?");
+            Console.Write(" a = ");
+            a = int.Parse(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("numarul {0}  = ", i);
+                nr = int.Parse(Console.ReadLine());
+
+                if ( nr == a)
+                {
+                    c = i;
+                }
+            }
+
+            c = -1 ; //contorul 
+
+            Console.WriteLine($"Numarul {a} se afla pe pozitia {c} din secventa. ");
+        }
+    }
+}
